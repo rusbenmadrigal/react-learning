85058cc77084b81b3315439a97a698f36fdf384c
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2023-03-05 17:35:10 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React JS
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React JS
+          </a>
+        </section>
+        <section>
+          <Russ msg="Hello I'm a props component" />
+        </section>
       </header>
-      <section>
-        <Russ msg="Hello I'm a props component" />
-      </section>
     </div>
   );
 }
